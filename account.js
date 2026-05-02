9f8b606aea46abe244139ef1d68acbc8ffe38f64
abdulhamid_al_mallah <abd49717@gmail.com>
2026-05-02 21:24:17 +0300

    add email filter

--- account.js
+++ account.js
@@ -1,5 +1,4 @@
 function validateAccount(firstName, lastName, email, dob, password, confirmPassword) {
-
    if (!firstName || !lastName || !email || !dob || !password || !confirmPassword) {
       return "All fields are required";
    }
@@ -7,10 +6,15 @@ function validateAccount(firstName, lastName, email, dob, password, confirmPassw
    if (firstName.length < 3 || lastName.length < 3) {
       return "First name and last name must be at least 3 characters long";
    }
-   
+
    const nameRegex = /^[a-zA-Z\s-]+$/;
    if (!nameRegex.test(firstName) || !nameRegex.test(lastName)) {
       return "Names should only contain letters";
+   }
+
+   const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+   if (!emailRegex.test(email)) {
+      return "Invalid email format";
    }
    return "Success";
 }
