769b2f234788eb08638ff3b897dfee926d4fab78
abdulhamid_al_mallah <abd49717@gmail.com>
2026-05-02 21:20:14 +0300

    validate all fields

--- account.js
+++ account.js
@@ -1,5 +1,9 @@
 function validateAccount(firstName, lastName, email, dob, password, confirmPassword) {
 
+   if (!firstName || !lastName || !email || !dob || !password || !confirmPassword) {
+      return "All fields are required";
+   }
+   
    return "Success";
 }
 
