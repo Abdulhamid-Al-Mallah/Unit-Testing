ac0b8fa3306c8b1d6cde744ec2706fce6135cadf
abdulhamid_al_mallah <abd49717@gmail.com>
2026-05-02 21:21:12 +0300

    validate Fname, Lname length

--- account.js
+++ account.js
@@ -3,7 +3,10 @@ function validateAccount(firstName, lastName, email, dob, password, confirmPassw
    if (!firstName || !lastName || !email || !dob || !password || !confirmPassword) {
       return "All fields are required";
    }
-   
+
+   if (firstName.length < 3 || lastName.length < 3) {
+      return "First name and last name must be at least 3 characters long";
+   }
    return "Success";
 }
 
